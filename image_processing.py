ba97eb08ffb87a754e73c01a6671b3a3c842d8ff
mabrao <mabrao@purdue.edu>
2021-12-01 13:02:34 +0000

    Changed the structure of the code so it is better designed:

--- image_processing.py
+++ image_processing.py
@@ -6,105 +6,97 @@ import numpy as np
 
 class ImageProcessing():
     def __init__(self):
-        self.images = []
         self.cvImages = []
         for filename in os.listdir('./images'):
-            self.images.append(filename)
-        for image in self.images:
-            self.cvImages.append(cv2.imread('./images/{}'.format(image))) #load and append image to cvImages
+            self.cvImages.append(cv2.imread('./images/{}'.format(filename))) #load and append image to cvImages
 
-    def threshold(self, imageList):
+    def threshold(self, image):
         '''
-        This method receives a list of images and
-        returns a binarised list of the images
+        This method receives a cv image as 
+        an argument and returns a thresholded image.
         '''
-        self.binarisedImages = []
-        #applying binarisation to all images:
-        self.kernel_size = 5
-        self.kernel = np.ones((self.kernel_size, self.kernel_size), np.uint8) #simple homogeneous kernel
-        for image in imageList:
-            #convert to greyscale
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            #selecting the binary inverted threshold (assigns ret to the threshold that was used and thresh to the thresholded image):
-            self.ret, self.threshImage = cv2.threshold(image, 180, 255, cv2.THRESH_BINARY)
-            self.binarisedImages.append(self.threshImage)
+        #convert to greyscale
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        #selecting the binary inverted threshold (assigns ret to the threshold that was used and thresh to the thresholded image):
+        ret, threshImage = cv2.threshold(image, 180, 255, cv2.THRESH_BINARY)
 
-        return self.binarisedImages
-
+        return threshImage
         
-    def OuterEdgeDetection(self): #this needs to be passed an argument when called for the images to be processed
-        self.contouredImages = []
-        self.contourPoints = []
+
+    def OuterEdgeDetection(self, image):
+        '''
+        This method takes a cv image as an argument and
+        returns a dictionary with the vertices for the image to be cropped.
+        '''
+        img_copy = image.copy()
         #finding only the external contours using cv2.RETR_EXTERNAL argument
-        for i, image in enumerate(self.cvImages):
-            img_copy = image.copy()
-            contours, hierarchy = cv2.findContours(self.binarisedImages[i], cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            contoured_img = cv2.drawContours(img_copy, contours, -1, (0, 255, 0), 2)
-            self.contouredImages.append(contoured_img)
-            self.contourPoints.append(contours)
-        #print(self.contourPoints) #debug
+        contours, hierarchy = cv2.findContours(self.threshold(image), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        self.contoured_img = cv2.drawContours(img_copy, contours, -1, (0, 255, 0), 2) #making this a self variable so it can be accessed on the show cards method
 
-        #calculations to find important information of contours
+        #calculations to find where to crop the card
         self.cropPositions = []
-        for contour in self.contourPoints:
-            for c in contour: #iterates through each point (x,y) of the contours array
-                [x,y,w,h] = cv2.boundingRect(c)
-                self.cropPositions.append([x,y,w,h])
+        for c in contours: #iterates through each point (x,y) of the contour array
+            [x,y,w,h] = cv2.boundingRect(c)
+            cropPositions = {'x':x,'y':y,'w':w,'h':h}
         
-        return self.contourPoints
+        return cropPositions
 
-
-    def cropCards(self):
-        self.croppedCards = []
-        for index, image in enumerate(self.cvImages):
-            x,y,w,h = self.cropPositions[index]
-            card = image[y:y+h, x:x+w]
-            self.croppedCards.append(card)
+    def cropCards(self, image):
+        '''
+        This method takes a cv image as an argument
+        and returns a cropped image with only the card.
+        '''
+        cropPositions = self.OuterEdgeDetection(image)
+        x,y,w,h = cropPositions['x'], cropPositions['y'], cropPositions['w'], cropPositions['h']
+        croppedCard = image[y:y+h, x:x+w]
         
-        return self.croppedCards
+        return croppedCard
     
 
-    def getCentralLogo(self):
+    def getCentralLogo(self, croppedCard):
         '''
-        This method returns the central logo for
-        all cards.
+        This method takes a cv cropped card as an argument
+        and returns the cropped central logo of the image.
         '''
-        self.central_logo = []
-        for card in (self.croppedCards):
+        #getting the card height and width:
+        height, width = croppedCard.shape[:2] 
 
-            #getting the card height and width:
-            height, width = card.shape[:2] 
+        #creating new variables for cropping relative to the size of the image
+        new_x = (width//3)
+        new_y = (height//3)
+        extra_size_x = width//12
+        extra_size_y = height//12
 
-            #creating new variables for cropping relative to the size of the image
-            new_x = (width//3)
-            new_y = (height//3)
-            extra_size_x = width//12
-            extra_size_y = height//12
-
-            #create new image with just the central information in the card
-            self.central_logo.append(card[(new_y - extra_size_y):(2*(new_y) + extra_size_y), (new_x - extra_size_x):(2*(new_x) + extra_size_x)])
+        #create new image with just the central information in the card
+        central_logo = (croppedCard[(new_y - extra_size_y):(2*(new_y) + extra_size_y), (new_x - extra_size_x):(2*(new_x) + extra_size_x)])
         
-        return self.central_logo
+        return central_logo
         
 
-    def getCardColor(self):
-        '''
-        This method will return a list with the
-        average color space for each card.
-        '''
+#     def getCardColor(self):
+#         '''
+#         This method will return a list with the
+#         average color space for each card.
+#         '''
 
-        color_spaces = []
-        #THESE FOR LOOPS NEED TO OPTIMIZED LATER
-        for card in self.croppedCards:
-            hsv_card = cv2.cvtColor(card, cv2.COLOR_BGR2HSV)
+#         hsv_cards = []
+#         for card in self.croppedCards:
+#             hsv_card = np.array(cv2.cvtColor(card, cv2.COLOR_BGR2HSV))
+#             hsv_card = hsv_card.reshape(-1,3) #converting to a 1D array
+#             for h,s,v in hsv_card:
+#                 print(h,s,v)
 
-            h,s,v = cv2.split(hsv_card)
+#             #h,s,v = (int((h/255.0)*100.0)), (int((s/255.0)*100.0)), (int((v/255.0)*100.0))
+            
+#             # for j in range(len(h)):
+#             #     print(h[i][j],s[i][j],v[i][j])
+#             #     h1,s1,v1 = (int((h[i][j]/255.0)*100.0)), (int((s[i][j]/255.0)*100.0)), (int((v[i][j]/255.0)*100.0))
+#             #     hsv_cards.append([h1, s1, v1])
 
-            for h1,s1,v1 in zip(h,s,v):
-                avg_h, avg_s, avg_v = (np.sum(h1))//len(h1), (np.sum(s1))//len(s1), (np.sum(v1))//len(v1)
-                color_spaces.append(np.array([avg_h, avg_s, avg_v]))
-
-        return color_spaces
+#             # h1,s1,v1 = (int((h/255.0)*100.0)), (int((s/255.0)*100.0)), (int((v/255.0)*100.0))
+#             # hsv_cards.append([h1, s1, v1])
+    
+#         return hsv_cards
 
 
     def showCards(self, imageNumber):
@@ -116,22 +108,24 @@ class ImageProcessing():
         to be shown.
         '''
         #Thresholding
-        cv2.imshow('Binarisation', self.binarisedImages[imageNumber])
+        cv2.imshow('Binarisation', self.threshold(self.cvImages[imageNumber]))
         cv2.waitKey(0)
         cv2.destroyWindow('Binarisation')
 
         #outer edge detection
-        cv2.imshow('Edge Detection', self.contouredImages[imageNumber])
+        self.OuterEdgeDetection(self.cvImages[imageNumber]) #perform edge detection on the selected image and store the contour image
+        cv2.imshow('Edge Detection', self.contoured_img)
         cv2.waitKey(0)
         cv2.destroyWindow('Edge Detection')
 
         #cropping the card on the outer edge
-        cv2.imshow('Cropped', self.croppedCards[imageNumber])
+        cv2.imshow('Cropped', self.cropCards(self.cvImages[imageNumber]))
         cv2.waitKey(0)
         cv2.destroyWindow('Cropped')
 
         #central logo of cards
-        cv2.imshow('Central Logo', self.central_logo[imageNumber])
+        croppedCard = self.cropCards(self.cvImages[imageNumber])
+        cv2.imshow('Central Logo', self.getCentralLogo(croppedCard))
         cv2.waitKey(0)
         cv2.destroyWindow('Central Logo')
 
